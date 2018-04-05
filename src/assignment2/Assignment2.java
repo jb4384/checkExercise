cd2107a960776b1fd5d47632e560bd5b02eb702f
Tiffany <tiffanyttran28@gmail.com>
2018-04-05 14:44:52 -0400

    Create Modularity Matrix B and working on finding the largest positive eigenvalue

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -17,7 +17,7 @@ public class Assignment2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double[][] d = {{0, 0, 1, 0, 0, 0, 1, 1, 1, 1}, //d = adjacent matrix
+        double[][] a = {{0, 0, 1, 0, 0, 0, 1, 1, 1, 1}, //d = adjacent matrix
         {0, 0, 1, 1, 0, 1, 1, 0, 0, 0},
         {1, 1, 0, 1, 1, 1, 0, 0, 0, 0},
         {0, 1, 1, 0, 1, 1, 0, 0, 1, 0},
@@ -27,12 +27,48 @@ public class Assignment2 {
         {1, 0, 0, 0, 0, 0, 1, 0, 1, 1},
         {1, 0, 0, 1, 0, 0, 0, 1, 0, 1},
         {1, 0, 0, 0, 0, 1, 1, 1, 1, 0}};
-        print(d);
-        char a = 'a';
-        for (int i = 0; i < d.length; i++) {
-            System.out.println("Test: " + a++ + " " + countOnes(d[i]));
+
+        int[] kj = new int[a.length]; // kj
+        int[] ki = new int[a.length]; // ki
+        int M = 0;
+        for (int i = 0; i < a.length; i++) {
+            int count = countOnes(a[i]);
+            M += count;
+            kj[i] = count;
+            ki[i] = count;
         }
 
+        // Build the modularity matrix B
+        double[][] b = new double[a.length][a.length];
+        for (int row = 0; row < a.length; row++) {
+            for (int col = 0; col < a.length; col++) {
+
+                /* In each cell, multiply sum of that cell's row in A by
+                sum of that cell's column in A. Divide result by M, the
+                total sum of A. Subtract this value P from corresponding 
+                value in A. */
+                double p = 1.0 * ki[row] * kj[col] / M;
+                b[row][col] = a[row][col] - p;
+            }
+        }
+        double eValue = eigen(b);
+        print(b);
+
+    }
+
+    /* Find the largest positive eigenvalue in matrix B. 
+    Create the identity matrix I * lamba. */
+    public static double eigen(double[][] b) {
+        /* The first bracket is the the length of the row for the matrix.
+        The second bracket is the number of columns. The third bracket is
+        to hold the values of the ordered pair. */
+        double[][][] r = new double[b.length][b.length][2];
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                
+            }
+        }
+        return 0;
     }
 
     public static int countOnes(double[] a) {
@@ -99,5 +135,35 @@ public class Assignment2 {
             System.out.println();
         }
     }
+    
+}
 
+class Polynomial {
+    
+    public double[] coef = {0};
+    
+    // Construct using a list of coefficients for terms n^0, n^1, n^2... 
+    public Polynomial(double[] params) {
+        if (coef.length == 0) coef = params;
+    }
+    
+    // Store a number as a polynomial
+    public Polynomial(double c) {
+        coef[0] = c;
+    }
+    
+    // Order of a polynomial (highest exponent)
+    public int order() {
+        return coef.length - 1;
+    }
+    
+    // Add 2 polynomials, return sum as new polynomial
+    public Polynomial add(Polynomial otherPoly) {
+        // Order of sum matches input with highest order.
+        int sumOrder = order();
+        if (order() > otherPoly.order()) {
+            sumOrder = otherPoly.order();
+        }
+        
+    }
 }

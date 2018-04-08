9e7d5530a5f4d26ad58d47f612970c836e3dd07b
Tiffany <tiffanyttran28@gmail.com>
2018-04-08 05:30:47 -0400

    Cleaned up un-used code and methods. Added Linear package to find eigenvector.

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -5,7 +5,14 @@
  */
 package assignment2;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import org.apache.commons.math3.linear.EigenDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 /**
  *
@@ -38,39 +45,81 @@ public class Assignment2 {
             ki[i] = count;
         }
 
-        // Build the modularity matrix B
-        double[][] b = new double[a.length][a.length];
+        /* Create the modularity matrix B (B = A - P) 
+        Matrix A is created above. Matrix P is P1 / M.
+        P1 is kj * ki.*/
+        double[][] B = new double[a.length][a.length];
         for (int row = 0; row < a.length; row++) {
             for (int col = 0; col < a.length; col++) {
-
                 /* In each cell, multiply sum of that cell's row in A by
                 sum of that cell's column in A. Divide result by M, the
                 total sum of A. Subtract this value P from corresponding 
                 value in A. */
                 double p = 1.0 * ki[row] * kj[col] / M;
-                b[row][col] = a[row][col] - p;
+                B[row][col] = a[row][col] - p;
             }
         }
-        double eValue = eigen(b);
-        print(b);
 
-    }
-
-    /* Find the largest positive eigenvalue in matrix B. 
-    Create the identity matrix I * lamba. */
-    public static double eigen(double[][] b) {
-        /* The first bracket is the the length of the row for the matrix.
-        The second bracket is the number of columns. The third bracket is
-        to hold the values of the ordered pair. */
-        double[][][] r = new double[b.length][b.length][2];
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                
+        RealMatrix m = MatrixUtils.createRealMatrix(B);
+        NumberFormat formatter = new DecimalFormat("#0.000");
+        System.out.println("Matrix B: ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.printf("%10s", formatter.format(m.getEntry(i, j)));
             }
+            System.out.println();
         }
-        return 0;
-    }
+        EigenDecomposition ev = new EigenDecomposition(m);
+        RealVector t = ev.getEigenvector(0);
 
+        System.out.println("This is the eigenvector at i: " + t);
+
+        /*Created two arrays: g1 and g2. g1 holds the vertices of the pos values
+        in the eigenvector. g2 holds the vetices of the neg values in the
+        eigenvector. Sg1 and Sg2 are vectors that will be used in calculating
+        the modularity value later.*/
+        int size = t.getDimension();
+        ArrayList<Integer> g1 = new ArrayList<>();
+        double[] Sg1 = new double[size];
+        ArrayList<Integer> g2 = new ArrayList<>();
+        double[] Sg2 = new double[size];
+        int count = 1;
+        for (int i = 0; i < size; i++) {
+            if (t.getEntry(i) > 0) {
+                g1.add(count);
+                Sg1[i] = 1;
+            } else if (t.getEntry(i) < 0) {
+                g2.add(count);
+                Sg2[i] = 1;
+            }
+            count++;
+        }
+
+        System.out.println("G1: " + g1);
+        System.out.println("G2: " + g2);
+        
+        /* Calculate the modularity value for each group g1 and g2.
+        Modulatiry value is Z = (1/(4m))(S^t)(BS), where m is M / 2, (M = 44)
+        S is the column vector of for group g1 or g2, t is transposition, and
+        B is the modularity matrix.*/
+        //G1:
+        double[] temp1 = multiplyVector(B, Sg1);
+        
+        //G2:
+        double[] temp2 = multiplyVector(B, Sg2);
+        
+        
+        
+        double Zg1;
+        double Zg2;
+        
+        /*If Z is less than or equal to zero, then the group is not required
+        to be divided further. Else, repeat the entire process for the
+        subgraph composed of the vertices in the group.*/
+
+    }
+    
+    // Counts the frequency of ones in an array
     public static int countOnes(double[] a) {
         int count = 0;
         for (int i = 0; i < a.length; i++) {
@@ -80,7 +129,8 @@ public class Assignment2 {
         }
         return count;
     }
-
+    
+    // Multiply two matrices together
     public static double[][] multiplyMatrices(double[][] a, double[][] b) {
         int m1 = a.length;
         int n1 = a[0].length;
@@ -99,7 +149,8 @@ public class Assignment2 {
         }
         return c;
     }
-
+    
+    // Multiplies a matrix with a vector
     public static double[] multiplyVector(double[][] a, double[] x) {
         int m = a.length;
         int n = a[0].length;
@@ -114,56 +165,21 @@ public class Assignment2 {
         }
         return y;
     }
-
-    public static double[][] transpose(double[][] a) {
-        int m = a.length;
-        int n = a[0].length;
-        double[][] b = new double[n][m];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                b[j][i] = a[i][j];
-            }
-        }
-        return b;
+    
+    //Multiplies a vector with a vector
+    public static double[] multiVect(double[] a, double[] b) {
+        
+        return null;
     }
 
+    //Prints a formatted 2D array as matrix
     public static void print(double[][] a) {
+        NumberFormat formatter = new DecimalFormat("#0.000");
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
-                System.out.print(a[i][j] + " ");
+                System.out.printf("%10s", formatter.format(a[i][j]));
             }
             System.out.println();
         }
-    }
-    
-}
-
-class Polynomial {
-    
-    public double[] coef = {0};
-    
-    // Construct using a list of coefficients for terms n^0, n^1, n^2... 
-    public Polynomial(double[] params) {
-        if (coef.length == 0) coef = params;
-    }
-    
-    // Store a number as a polynomial
-    public Polynomial(double c) {
-        coef[0] = c;
-    }
-    
-    // Order of a polynomial (highest exponent)
-    public int order() {
-        return coef.length - 1;
-    }
-    
-    // Add 2 polynomials, return sum as new polynomial
-    public Polynomial add(Polynomial otherPoly) {
-        // Order of sum matches input with highest order.
-        int sumOrder = order();
-        if (order() > otherPoly.order()) {
-            sumOrder = otherPoly.order();
-        }
-        
     }
 }
